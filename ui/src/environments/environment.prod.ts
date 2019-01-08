f4bf24c6fdee5feacdd6486d0e87a2f11f227265
Caleb Hiebert <calebkhiebert@gmail.com>
2019-01-08 15:08:29 -0600

    ci init

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   auth0Domain: 'panch-dd.auth0.com',
   auth0ClientId: '9vKGU44oDoiGxliIyFL7hjWQQSdmvWEz',
+  apiURL: 'https://dd.panchem.io/api',
 };
