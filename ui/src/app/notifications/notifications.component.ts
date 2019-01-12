d84f61ae9033f203d2aece39978218ed20d60a77
Caleb Hiebert <calebkhiebert@gmail.com>
2019-01-12 14:00:18 -0600

    nav changes

--- ui/src/app/notifications/notifications.component.ts
+++ ui/src/app/notifications/notifications.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NotificationService } from '../notification.service';
+import { NotificationService, Notification } from '../notification.service';
 
 @Component({
   selector: 'dd-notifications',
