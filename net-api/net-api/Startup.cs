6fd755dc837c29b3c13e879109ca1c00af48f74c
Caleb Hiebert <calebkhiebert@gmail.com>
2019-01-22 15:55:05 -0600

    fix crash error

--- net-api/net-api/Startup.cs
+++ net-api/net-api/Startup.cs
@@ -26,7 +26,7 @@ namespace net_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            DotEnv.Config();
+            DotEnv.Config(throwOnError: false);
 
             var context = new Context();
             context.Database.Migrate();
