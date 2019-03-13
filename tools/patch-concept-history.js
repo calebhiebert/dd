d58e3846f0bfd7672a8b1f6e5f76a056ba814711
Caleb Hiebert <calebkhiebert@gmail.com>
2019-03-12 21:09:50 -0500

    create script to patch in existing history values

---
+++ tools/patch-concept-history.js
@@ -0,0 +1,37 @@
+const { Client } = require('pg');
+const uuid = require('uuid/v4');
+
+const db = new Client({
+  host: 'localhost',
+  database: 'dd',
+  user: 'dd',
+  password: 'dd',
+});
+
+async function patch() {
+  await db.connect();
+
+  let idx = 0;
+
+  const { rows } = await db.query(`SELECT * FROM "Concepts"`);
+
+  for (const row of rows) {
+    const history = await db.query(
+      `SELECT * FROM "ConceptHistories" WHERE "ConceptId" = '${row.Id}'`
+    );
+
+    if (history.rows.length === 0) {
+      // Create History
+      await db.query(`
+      INSERT INTO "ConceptHistories" 
+        ("Id", "UserId", "DateTime", "ActionType", "ActionSource", "ConceptId", "Name", "Content", "ImageId", "Fields", "Tags", "ConceptTypeId")
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`);
+    }
+
+    console.log(++idx, '/', rows.length);
+  }
+
+  await db.end();
+}
+
+patch();
