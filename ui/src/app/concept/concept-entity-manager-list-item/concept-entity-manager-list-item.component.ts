51131964b49202a13e5db3b9700cf8f4be696833
Caleb Hiebert <calebkhiebert@gmail.com>
2019-03-16 14:22:18 -0500

    split list items to their own component

--- ui/src/app/concept/concept-entity-manager-list-item/concept-entity-manager-list-item.component.ts
+++ ui/src/app/concept/concept-entity-manager-list-item/concept-entity-manager-list-item.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { IConceptEntity, IConceptType } from 'src/app/concept.service';
+import { CampaignService } from 'src/app/campaign.service';
 
 @Component({
   selector: 'dd-concept-entity-manager-list-item',
   templateUrl: './concept-entity-manager-list-item.component.html',
-  styleUrls: ['./concept-entity-manager-list-item.component.css']
+  styleUrls: ['./concept-entity-manager-list-item.component.css'],
 })
 export class ConceptEntityManagerListItemComponent implements OnInit {
+  @Input()
+  public conceptEntity: IConceptEntity;
 
-  constructor() { }
+  @Input()
+  public conceptType: IConceptType;
 
-  ngOnInit() {
+  @Input()
+  public draggable: boolean;
+
+  @Output()
+  public selected = new EventEmitter<IConceptEntity>();
+
+  constructor(private campaignService: CampaignService) {}
+
+  ngOnInit() {}
+
+  public select(conceptEntity: IConceptEntity) {
+    this.selected.emit(conceptEntity);
   }
 
+  public get campaign() {
+    return this.campaignService.campaign;
+  }
+
+  public get quantityEnabled() {
+    return this.conceptType.entityConfig.enabled && this.conceptType.entityConfig.enableQuantity;
+  }
 }
