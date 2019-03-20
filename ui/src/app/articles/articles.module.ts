f0a113b5b7303a71a1741b374e371b32b6737011
Caleb Hiebert <calebkhiebert@gmail.com>
2019-03-20 10:27:02 -0500

    remove article view mini

--- ui/src/app/articles/articles.module.ts
+++ ui/src/app/articles/articles.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { ArticleEditorComponent } from './article-editor/article-editor.component';
 import { ArticleManagerComponent } from './article-manager/article-manager.component';
 import { ArticleSelectComponent } from './article-select/article-select.component';
 import { ArticleViewComponent } from './article-view/article-view.component';
 import { ArticleViewCardComponent } from './article-view-card/article-view-card.component';
-import { ArticleViewMiniComponent } from './article-view-mini/article-view-mini.component';
 import { ArticleConceptManagerComponent } from './article-concept-manager/article-concept-manager.component';
 import { ArticleConceptManagerListItemComponent } from './article-concept-manager-list-item/article-concept-manager-list-item.component';
 import { ArticleQuestManagerComponent } from './article-quest-manager/article-quest-manager.component';
@@ -23,12 +21,10 @@ import { CustomViewsModule } from '../custom-views/custom-views.module';
     ArticleSelectComponent,
     ArticleViewComponent,
     ArticleViewCardComponent,
-    ArticleViewMiniComponent,
     ArticleConceptManagerComponent,
     ArticleConceptManagerListItemComponent,
     ArticleQuestManagerComponent,
   ],
   imports: [CommonModule, ArticlesRoutingModule, CustomControlsModule, NotesModule, ReactiveFormsModule, CustomViewsModule],
-  exports: [ArticleViewMiniComponent],
 })
 export class ArticlesModule {}
