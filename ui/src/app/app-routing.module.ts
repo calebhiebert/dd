3531d1540f73e00c10c3e9c4a30d833a085a1d31
Caleb Hiebert <calebhiebert@users.noreply.github.com>
2021-02-24 11:10:22 -0600

    upgrade to angular 11

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -263,7 +263,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
